6b6bbec63526ffe74c7406a7dab6765443c4b5d7
ilyas0707 <48711796+ilyas0707@users.noreply.github.com>
2019-04-05 00:11:08 +0600

    poh

--- page1/js/script.js
+++ page1/js/script.js
@@ -1,8 +1,26 @@
-let btn = document.querySelector('.triangle'),
-	menu = document.querySelector('.tape_bar'),
-	tape = document.querySelector('.tape');
+let d = document;
+const toggle_btn = d.querySelector('.hamburger'),
+	  menu = d.querySelector('.tape_bar'),
+	  header_nav = d.querySelector('.tape_menu');
 
-btn.addEventListener('click', function(){
-	menu.classList.toggle('open');
-	tape.classList.toggle('right')
-});
+
+
+var menu_active = false
+toggle_btn.addEventListener('click', toggle_menu);
+
+
+function toggle_menu() {
+	if(menu_active) {
+		menu.classList.remove('active');
+		this.classList.remove('active');
+		menu_active = false
+		header_nav.classList.remove('active');
+	} else {
+		menu.classList.add('active');
+		this.classList.add('active');
+		menu_active = true
+		header_nav.classList.add('active');
+
+	}
+}
+
